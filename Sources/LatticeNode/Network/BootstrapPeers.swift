4a7b18cb87060a6a5ff59da8b3164497fcd0e715
Ada Lin <ada_lin_lattice@proton.me>
2026-06-19 03:23:04 -0500

    network: seed mainnet bootstrap peers for public discovery

--- Sources/LatticeNode/Network/BootstrapPeers.swift
+++ Sources/LatticeNode/Network/BootstrapPeers.swift
@@ -3,7 +3,9 @@ import Ivy
 
 public enum BootstrapPeers {
     public static let nexus: [PeerEndpoint] = [
-
+        PeerEndpoint(publicKey: "ed019cb680d6179870415cd1ef60f607bbd0b9d55717205e512b9e5747fb02e9ad0b", host: "137.66.36.149", port: 4001),
+        PeerEndpoint(publicKey: "ed0199d490f0f460cdee13cdaec839fdc11d90ef47209282bde12493190c9598c39e", host: "137.66.14.188", port: 4001),
+        PeerEndpoint(publicKey: "ed01ce0dbd7bcf615782bb8d180b967ec00cc948e85a2e2a864b290a081cf6f8f575", host: "137.66.16.78", port: 4001),
     ]
 
     public static let testnet: [PeerEndpoint] = [
